72043e6e3c6b95634dc62cc635ea865dbc2db3e4
reinaldojfreitas <reinaldo.freitas@rj.senac.br>
2022-02-25 15:35:38 -0300

    Voltando para a linha 1

--- hellogit/src/hellogit/HelloGit.java
+++ hellogit/src/hellogit/HelloGit.java
@@ -4,11 +4,6 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Linha 1");
-		System.out.println("Linha 2");
-		System.out.println("Linha 3");
-		System.out.println("Linha 4");
-		System.out.println("Linha 5");
-		System.out.println("Linha 6");
 	}
 
 }
